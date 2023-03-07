5574ff7fb2fcb1444a1dd5d56c4199791763a035
Kushal Kumarasinghe <kushal.k@hotmail.com>
2023-03-07 01:51:19 -0500

    separate logic for getting transaction data and displaying it

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+let transactionData = [];
+
 function parseHTML() {
   // Create a new HTML document
   const html = $('#htmlInput').val();
@@ -10,29 +12,49 @@ function parseHTML() {
   outputDiv =  $('#output')
 
   // If a table was found, parse its data
+
   if (table) {
     console.log('yes we did')
-    // Create a new table element in the outputDiv
-    const outputTable = $("<table>");
-    outputDiv.append(outputTable);
-
+    console.log(transactionData)
+  
     // Get the table rows from the table element
     const rows = $(table).find("tr");
-
-    // Loop through each row and create a new row in the output table
+  
+    // Loop through each row and create a new row object
     rows.each(function() {
-      const outputRow = $("<tr>");
-      $(outputTable).append(outputRow);
-
-      // Get the cells from the row and create new cells in the output row
+      let rowObj = {}; // create empty object to hold row data
       const cells = $(this).find("td, th");
-      cells.each(function() {
-        const outputCell = $("<td>").text($(this).text());
-        $(outputRow).append(outputCell);
+      
+      cells.each(function(index) {        
+        // store cell data in rowObj
+        if (index === 0) {
+          rowObj.date = $(this).text().trim();
+        } else if (index === 1) {
+          rowObj.name = $(this).text().trim();
+        } else if (index === 2) {
+          rowObj.amount = $(this).text().trim();
+        }
       });
+      transactionData.push(rowObj); // add rowObj to transactionData array
     });
   }
+  console.log(transactionData)
+  createTable(transactionData)
 }
 
-
-
+function createTable(data) {
+  const outputTable = $("<table>");
+  const headerRow = $("<tr>");
+  const headerCells = $("<th>").text("Date").add($("<th>").text("Name")).add($("<th>").text("Amount"));
+  headerRow.append(headerCells);
+  outputTable.append(headerRow);
+  
+  for (let i = 0; i < data.length; i++) {
+    const row = $("<tr>");
+    const cells = $("<td>").text(data[i].date).add($("<td>").text(data[i].name)).add($("<td>").text(data[i].amount));
+    row.append(cells);
+    outputTable.append(row);
+  }
+  
+  $('#output').empty().append(outputTable);
+}
